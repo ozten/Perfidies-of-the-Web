10a79b1be4a7e1ca1fb4cb747036b86de4d622f7
Austin King <shout@ozten.com>
2009-08-23 09:53:58 -0700

    JSLint udpates

--- perfidies.js
+++ perfidies.js
@@ -1,14 +1,11 @@
 /* perfidies.js
-  
   There are two layers - The UI and the PFS2 API
   
   This file will (evetually) only host the PFS API. Other JS files will host
   the MoCo whatsnew UI, the SFx Up Your Plug Badges, etc
   
 */
-function compare(v1, v2) {
-    var v1c = parseVersion(v1);
-}
+
 /**
  * Ghetto BNF:
  * A Version = description? version comment?
@@ -18,18 +15,48 @@ function compare(v1, v2) {
  * seperator = .
  * 
  * v - string like "Quicktime 3.0.12"
- * return a VersionChain example - [3, 0, 12]
+ * return a "VersionChain" which is an array of version parts example - [3, 0, 12]
  */
-function parseVersion(v) {
+var parseVersion = function(v) {
     var tokens = v.split(' ');
+    var versionChain = [];
+
     var inVersion = false;
     var inNumericVersion = false;
     var inCharVersion = false;
-    var versionChain = [];
+
     var currentVersionPart = "";
+    function isNumeric(c) { return ! isNaN(parseInt(c, 10)); }
+    
+    function isSeperator(c) { return c === '.'; }
+
+    function startVersion(token, j) {
+        if (isNumeric(token[j])) {
+            inVersion = true;
+            inNumericVersion = true;
+            currentVersionPart += token[j];
+        } /* else {
+            skip we are in the description
+        }*/
+    }
+    
+    function finishVersionPart() {
+        //cleanup this versionPart        
+        if (inNumericVersion) {
+            versionChain.push(parseInt(currentVersionPart, 10));
+            inNumericVersion = false;
+        } else if (inCharVersion) {
+            versionChain.push(currentVersionPart);
+            inCharVersion = false;
+        } else {
+            if (window.console) { console.error("This should never happen", currentVersionPart, inNumericVersion, inCharVersion); }
+        }        
+        currentVersionPart = "";
+    }
+    
     for(var i=0; i < tokens.length; i++){
         var token = tokens[i].trim();
-        if (token.length == 0) {
+        if (token.length === 0) {
             continue;
         }
         for(var j=0; j < token.length; j++) {            
@@ -53,39 +80,17 @@ function parseVersion(v) {
         }
     }
     if (! inVersion) {        
-        if (window.console) console.warn("Unable to parseVersion from " + v);
+        if (window.console) { console.warn("Unable to parseVersion from " + v); }
     }
-    return versionChain;
-    function startVersion(token, j) {
-        if (isNumeric(token[j])) {
-            inVersion = true;
-            inNumericVersion = true;
-            currentVersionPart += token[j];
-        } /* else {
-            skip we are in the description
-        }*/
-    }
-    function finishVersionPart() {
-        //cleanup this versionPart        
-        if (inNumericVersion) {
-            versionChain.push(parseInt(currentVersionPart));
-            inNumericVersion = false;
-        } else if (inCharVersion) {
-            versionChain.push(currentVersionPart);
-            inCharVersion = false;
-        } else {
-            if (window.console) console.error("This should never happen", currentVersionPart, inNumericVersion, inCharVersion);
-        }        
-        currentVersionPart = "";
-    }
-}
+    return versionChain;    
+};
 
-function isNumeric(c) { return ! isNaN(parseInt(c)); }
-function isSeperator(c) { return c === '.'; }
 function pluginData() {
     
 }
-
+function compare(v1, v2) {
+    var v1c = parseVersion(v1);
+}
 var findPluginQueue = [];
 
 var currentPlugins = [];
