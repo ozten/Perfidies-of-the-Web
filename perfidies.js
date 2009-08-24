0c95f17846bdb6158002bcd347609e1a60fb99da
Austin King <shout@ozten.com>
2009-08-24 07:53:23 -0700

    Added compareVersion, compareVersionChain

--- perfidies.js
+++ perfidies.js
@@ -5,107 +5,148 @@
   the MoCo whatsnew UI, the SFx Up Your Plug Badges, etc
   
 */
-
-/**
- * Ghetto BNF:
- * A Version = description? version comment?
- * version = versionPart | versionChain
- * versionPart = digit | character
- * versionChain = versionPart (seperator versionPart)+
- * seperator = .
- * 
- * v - string like "Quicktime 3.0.12"
- * return a "VersionChain" which is an array of version parts example - [3, 0, 12]
- */
-var parseVersion = function(v) {
-    var tokens = v.split(' ');
-    var versionChain = [];
-
-    var inVersion = false;
-    var inNumericVersion = false;
-    var inCharVersion = false;
-
-    var currentVersionPart = "";
-    function isNumeric(c) { return ! isNaN(parseInt(c, 10)); }
+var Pfs = {
+    /**
+     * Compares the description of two plugin versions and returns
+     * either 1, 0, or -1 to indicate:
+     * newer = 1
+     * same = 0
+     * older = -1
+     * @param plugin1 {string} The first plugin description. Example: QuickTime Plug-in 7.6.2
+     * @param plugin2 {string} The second plugin description to compare against
+     * @returns {integer} The comparison results
+     */
+    compVersion: function(v1, v2) {
+        return this.compVersionChain( this.parseVersion(v1),
+                                      this.parseVersion(v2));
+    },
     
-    function isChar(c) { return "abcdefghijklmnopqrstuvwxyz".indexOf(c.toLowerCase())  >= 0; }
+    /**
+     * Ghetto BNF:
+     * A Version = description? version comment?
+     * version = versionPart | versionChain
+     * versionPart = digit | character
+     * versionChain = versionPart (seperator versionPart)+
+     * seperator = .
+     * 
+     * v - string like "Quicktime 3.0.12"
+     * return a "VersionChain" which is an array of version parts example - [3, 0, 12]
+     */
+     parseVersion: function(v) {
+        var tokens = v.split(' ');
+        var versionChain = [];
     
-    function isSeperator(c) { return c === '.'; }
-
-    function startVersion(token, j) {
-        if (isNumeric(token[j])) {
-            inVersion = true;
-            inNumericVersion = true;
-            currentVersionPart += token[j];
-        } /* else {
-            skip we are in the description
-        }*/
-    }
+        var inVersion = false;
+        var inNumericVersion = false;
+        var inCharVersion = false;
     
-    function finishVersionPart() {
-        //cleanup this versionPart        
-        if (inNumericVersion) {
-            versionChain.push(parseInt(currentVersionPart, 10));
-            inNumericVersion = false;
-        } else if (inCharVersion) {
-            versionChain.push(currentVersionPart);
-            inCharVersion = false;
-        } else {
-            if (window.console) { console.error("This should never happen", currentVersionPart, inNumericVersion, inCharVersion); }
-        }        
-        currentVersionPart = "";
-    }
+        var currentVersionPart = "";
+        function isNumeric(c) { return ! isNaN(parseInt(c, 10)); }
+        
+        function isChar(c) { return "abcdefghijklmnopqrstuvwxyz".indexOf(c.toLowerCase())  >= 0; }
+        
+        function isSeperator(c) { return c === '.'; }
     
-    for(var i=0; i < tokens.length; i++){
-        var token = tokens[i].trim();
-        if (token.length === 0) {
-            continue;
+        function startVersion(token, j) {
+            if (isNumeric(token[j])) {
+                inVersion = true;
+                inNumericVersion = true;
+                currentVersionPart += token[j];
+            } /* else {
+                skip we are in the description
+            }*/
         }
-        for(var j=0; j < token.length; j++) {            
-            if (inVersion) {
-                if (isNumeric(token[j])) {
-                    if (inCharVersion) {
-                        finishVersionPart();
-                    }
-                    inNumericVersion = true;
-                    currentVersionPart += token[j];                
-                } else if(j != 0 && isChar(token[j])) {
-                    //    j != 0 - We are mid-token right? 3.0pre OK 3.0 Pre BAD
-                    if (inNumericVersion) {
-                        finishVersionPart();
-                    }
-                    inCharVersion = true;
-                    currentVersionPart += token[j];
-                } else if(isSeperator(token[j])) {
-                    finishVersionPart();
-                } else {
-                    return versionChain;
-                }
+        
+        function finishVersionPart() {
+            //cleanup this versionPart        
+            if (inNumericVersion) {
+                versionChain.push(parseInt(currentVersionPart, 10));
+                inNumericVersion = false;
+            } else if (inCharVersion) {
+                versionChain.push(currentVersionPart);
+                inCharVersion = false;
             } else {
-                startVersion(token, j);
+                if (window.console) { console.error("This should never happen", currentVersionPart, inNumericVersion, inCharVersion); }
+            }        
+            currentVersionPart = "";
+        }
+        
+        for(var i=0; i < tokens.length; i++){
+            var token = tokens[i].trim();
+            if (token.length === 0) {
+                continue;
+            }
+            for(var j=0; j < token.length; j++) {            
+                if (inVersion) {
+                    if (isNumeric(token[j])) {
+                        if (inCharVersion) {
+                            finishVersionPart();
+                        }
+                        inNumericVersion = true;
+                        currentVersionPart += token[j];                
+                    } else if(j != 0 && isChar(token[j])) {
+                        //    j != 0 - We are mid-token right? 3.0pre OK 3.0 Pre BAD
+                        if (inNumericVersion) {
+                            finishVersionPart();
+                        }
+                        inCharVersion = true;
+                        currentVersionPart += token[j];
+                    } else if(isSeperator(token[j])) {
+                        finishVersionPart();
+                    } else {
+                        return versionChain;
+                    }
+                } else {
+                    startVersion(token, j);
+                }
+            }
+            if (inVersion) {
+                //clean up previous token
+                finishVersionPart();
             }
         }
-        if (inVersion) {
-            //clean up previous token
-            finishVersionPart();
+        if (! inVersion) {        
+            if (window.console) { console.warn("Unable to parseVersion from " + v); }
         }
+        return versionChain;    
+    },
+    /**
+     * Given two "version chains" it determines if the first is newer, the same, or older
+     * than the second argument. The results is either 1, 0, or -1
+     * newer = 1
+     * same = 0
+     * older = -1
+     * @param versionChain1 {array} A list of version components Example [5, 3, 'a']
+     * @param versionChain2 {array} The other version chain to compare against
+     * @returns integer
+     */
+    compVersionChain: function(vc1, vc2) {
+        for(var i=0; i < vc1.length && i < vc2.length; i++) {
+            if (vc1[i] != vc2[i]) {
+                if (vc1[i] > vc2[i]) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        }
+        if (vc1.length != vc2.length) {
+            if (vc1.length > vc2.length) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+        return 0;
+    },
+    pluginData: function() {
+        
     }
-    if (! inVersion) {        
-        if (window.console) { console.warn("Unable to parseVersion from " + v); }
-    }
-    return versionChain;    
-};
-
-function pluginData() {
-    
 }
-function compare(v1, v2) {
-    var v1c = parseVersion(v1);
-}
-var findPluginQueue = [];
-
-var currentPlugins = [];
-var outdatedPlugins = [];
-var vulnerablePlugins = [];
-var unknownPlugins = [];
-
+/*
+  findPluginQueue: [],    
+    currentPlugins: [],
+    outdatedPlugins: [],
+    vulnerablePlugins: [],
+    unknownPlugins: []
+*/
